90afd320c5949f6d5f271d21e3ff593472f45263
Mannat <mannattuli13@gmail.com>
2021-11-28 10:14:56 +0530

    pls work

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
   before_action :set_list, only: %i[ show edit update destroy ]
 
-  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :authenticate_user!, except: [:index, :show]
 
   before_action :correct_user, only: [:edit, :update, :destroy]
 
